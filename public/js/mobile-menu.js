88a7862e88f0e69d6690f0ea088d6875f764664f
bigtank <codeplay.erniie@gmail.com>
2023-05-27 20:24:22 +0100

    Built files

--- public/js/mobile-menu.js
+++ public/js/mobile-menu.js
@@ -3,11 +3,10 @@ export function mobileMenu() {
 
   if (block) {
     let hamburger = block.querySelector(".hamburger");
-    // On click
+    
     hamburger.addEventListener("click", function() {
-      // Toggle class "is-active"
       hamburger.classList.toggle("is-active");
-      // Do something else, like open/close menu
+      
     });
   }
 
